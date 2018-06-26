4816ecb15c9d38fe4feccf2e772b8abbdc6e568a
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-26 00:56:53 -0700

    setInterval works; setTimer needs to be reversed?

--- timer.js
+++ timer.js
@@ -3,48 +3,71 @@
  Squares are eligible for a color change if its color has not changed in the last two seconds.
 */
 const timeInt = 250;
-const timeDelay = 2000;
+const timeDelay = 300;
 const color1 = 'mediumvioletred';
 const color2 = 'teal';
 let intID;
 
-function getRandomCell() {
+function getRandomCell () {
   const min = Math.ceil(1);
   const max = Math.floor(16);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function changeClass(c) {
-  c.className = "ineligible";
-  console.log("classname inside setTimeout: " +c.className);
+function changeClass (c) {
+  // set timer to wait 2 seconds before changing class to "eligible"
+  // change class to "eligible"?
+  console.log(c);
+  let timeOutID = setTimeout(changeBack, timeDelay);
+
+  function changeBack () {
+    if (c.className == "ineligible") {
+      c.className = "eligible";
+      clearInterval(timeOutID);
+    } else {
+      c.className = "ineligible";
+      console.log("classname inside setTimeout: " +c.className);
+    }
+  }
+
+  // clearInterval(timeOutID);
 }
 
-function changeBkgColor() {
+function changeBkgColor () {
   const cell = document.getElementById(getRandomCell());
 
   if (cell.className == "eligible") {
 
     cell.style.backgroundColor = cell.style.backgroundColor == color1 ? color2 : color1;
 
-    // set timer to wait 2 seconds before changing class to "eligible"
-    // change class to "eligible"?
-    // let timeOutID = setTimeout(function() {
-      changeClass(cell);
-    // }, timeDelay);
 
     console.log("className inside if: " +cell.className);
-
-    // clearInterval(timeOutID);
-
-
   }
+  changeClass(cell);
   console.log("className outside: " +cell.className);
 }
 
-function changeColor() {
+function changeColor () {
   intID = setInterval(changeBkgColor, timeInt);
 }
 
-function stopInterval() {
+function stopInterval () {
   clearInterval(intID);
+}
+
+
+function move() {
+  var elem = document.getElementById("moveBar");
+  var width = 0;
+  var idVar = setInterval(change, 10);
+
+  function change() {
+    if (width == 100){
+       clearInterval(idVar)
+    }
+    else {
+       width++;
+       elem.style.width = width + '%';
+    }
+  }
 }
