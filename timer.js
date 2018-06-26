7a642cc0419eeeb5100bd9ee78ebe10e0809ce56
ThuyNT13 <ngo.thuy38@gmail.com>
2018-06-26 02:21:05 -0700

    Clean up code

--- timer.js
+++ timer.js
@@ -1,12 +1,25 @@
 /*
- Every 250 milliseconds, one of the eligible squares should be randomly selected to change colors.
- Squares are eligible for a color change if its color has not changed in the last two seconds.
+Please create a web page that has a 4x4 grid, with a border around it. The grid should be horizontally centered in the web page. Every 250 milliseconds, one of the eligible squares should be randomly selected to change colors. Squares are eligible for a color change if its color has not changed in the last two seconds.
+
+Feel free to conduct research on the web, but do not ask anyone for help. Also, do not use any libraries (e.g. jQuery) or any publishing software such as Microsoft Word. If you’re unable to create this page (even after consulting the web), this role probably won’t be a good fit. We encourage you to spend some time to verify the logic in your solution.
+
+[x] - 4 x 4 grid
+  [x] - border
+  [x] - centered, horizontally
+[x] - change background color for cell
+  [x] - setInterval timer cycle for 250 ms
+  [x] - randomize cell
+  [x] - set className to "ineligible"
+    [x] - setTimeout timer cycle for 2000ms
+[x] - change className back to "eligible"
+  [x] - clearTimeout
 */
 const timeInt = 250;
 const timeDelay = 300;
 const color1 = 'mediumvioletred';
 const color2 = 'teal';
 let intID;
+let timeOutID;
 
 function getRandomCell () {
   const min = Math.ceil(1);
@@ -15,35 +28,24 @@ function getRandomCell () {
 }
 
 function changeClass (c) {
-  // set timer to wait 2 seconds before changing class to "eligible"
-  // change class to "eligible"?
-  console.log(c);
-  let timeOutID = setTimeout(changeBack, timeDelay);
+  timeOutID = setTimeout(changeBack, timeDelay);
 
   function changeBack () {
     if (c.className == "ineligible") {
       c.className = "eligible";
-      clearInterval(timeOutID);
+      clearTimeout(timeOutID);
     }
-      // c.className = "ineligible";
-      console.log("classname inside setTimeout: " +c.className);
   }
-
-  // clearInterval(timeOutID);
 }
 
 function changeBkgColor () {
   const cell = document.getElementById(getRandomCell());
 
   if (cell.className == "eligible") {
-
     cell.style.backgroundColor = cell.style.backgroundColor == color1 ? color2 : color1;
     cell.className = "ineligible";
-
-    console.log("className inside if: " +cell.className);
   }
   changeClass(cell);
-  console.log("className outside: " +cell.className);
 }
 
 function changeColor () {
@@ -52,21 +54,4 @@ function changeColor () {
 
 function stopInterval () {
   clearInterval(intID);
-}
-
-
-function move() {
-  var elem = document.getElementById("moveBar");
-  var width = 0;
-  var idVar = setInterval(change, 10);
-
-  function change() {
-    if (width == 100){
-       clearInterval(idVar)
-    }
-    else {
-       width++;
-       elem.style.width = width + '%';
-    }
-  }
 }
